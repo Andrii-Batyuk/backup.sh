1642eda3c32707e1426fdd338c2e6a9048a31d27
Andrii Batyuk <andrii.batyuk@gmail.com>
2020-06-30 16:22:46 +0300

    qwe

--- backup.sh
+++ backup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo "Hello, World!"
 
+<<<<<<< Updated upstream
 if [ 1 = 1]
+=======
+if [ 1 eq 1];
+>>>>>>> Stashed changes
 
 then 
     echo "Is equal"
 else
     echo "Is not equal"
 fi
+123
